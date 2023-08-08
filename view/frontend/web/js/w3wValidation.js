211a69d8c71b38ef4b7ef602ad8254cf84db1c41
Chris Shaw <7129522+c5haw@users.noreply.github.com>
2023-08-08 18:01:07 +0100

    Add comment hint to global function for this

--- view/frontend/web/js/w3wValidation.js
+++ view/frontend/web/js/w3wValidation.js
@@ -6,6 +6,7 @@ define(['jquery'], function ($) {
         $.validator.addMethod(
             'validate-w3w',
             function(value) {
+                // This can be updated to use the window.what3words.utils.validateAddress function instead so we don't another source of truth to update when we want to update RegEx.
                 return /^\/{0,}[^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[・.。][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}[・.。][^0-9`~!@#$%^&*()+\-_=[{\]}\\|'<,.>?/";:£§º©®\s]{1,}$/i.test(value);
             },
             $.mage.__("What3Words address is invalid.")
